6847c8ba50e50a6ec414b0bcffbf001ebd8af544
마현우 <mhw35689402@gmail.com>
2023-07-13 15:55:47 +0900

    crud - r

--- first-prisma/src/prisma/prisma.service.ts
+++ first-prisma/src/prisma/prisma.service.ts
@@ -1,16 +1,5 @@
-import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
+import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class PrismaService
-  extends PrismaClient
-  implements OnModuleInit, OnModuleDestroy
-{
-  async onModuleInit() {
-    await this.$connect();
-  }
-
-  async onModuleDestroy() {
-    await this.$disconnect();
-  }
-}
+export class PrismaService extends PrismaClient {}
---
+++ first-prisma/src/user/user.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class UserService {
+  constructor(private prismaService: PrismaService) {}
+
+  async findAll() {
+    return await this.prismaService.user.findMany();
+  }
+
+  async findOne(id: number) {
+    return await this.prismaService.user.findUnique({
+      where: { id: id },
+    });
+  }
+}
